d98408de284c0158eb42eb645d7f5f752240e757
JEPITT-PC\M Hafidz Ilham <mhafidzilham@gmail.com>
2018-06-24 14:43:01 +0700

    tambah controller

---
+++ application/controllers/Kamera.php
@@ -0,0 +1,17 @@
+<?php  
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kamera extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Kamera.php */
+/* Location: ./application/controllers/Kamera.php */
+
+?>
