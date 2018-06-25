dd202db2e3ae2d0b780cd3745de20f09eed93874
JEPITT-PC\M Hafidz Ilham <mhafidzilham@gmail.com>
2018-06-25 10:47:15 +0700

    nambah model user

---
+++ application/models/User.php
@@ -0,0 +1,14 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Model {
+
+	
+
+}
+
+/* End of file User.php */
+/* Location: ./application/models/User.php */
+
+ ?>
