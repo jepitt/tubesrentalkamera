63cec3383537ebacc40d8933e78f8221646dcb7a
HAIDARHAMMAM\haidarhammam <hamamhaidar89@gmail.com>
2018-07-01 21:49:53 +0700

    insert model

--- application/models/User.php
+++ application/models/User.php
@@ -1,14 +1,35 @@
-<?php 
+ <?php
+	 defined('BASEPATH') OR exit('No direct script access allowed');
+	 
+	 class User extends CI_Model
+	 {
+	 	public function login($username,$password)
+	 	{
+	 		$this->db->select('id,username,password,level');
+	 		$this->db->from('user');
+	 		$this->db->where('username',$username);
+	 		$this->db->where('password', MD5($password));
+	 		$query = $this->db->get();
+	 		if($query->num_rows()==1){
+	 			return $query->result();
+	 		}else{
+	 			return false;
+	 		}
+	 		
+	 	}
+	 
+	 	
+	 	public function insert()
+	 	{
+	 		$data = array(
+	 			'username' => $this->input->post('username'),
+	 			'password' => md5($this->input->post('password')),
+	 			'level' => $this->input->post('level')
+	 		);
+	 		$this->db->insert('user',$data);
+	 	}
+	 }
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class User extends CI_Model {
-
-	
-
-}
-
-/* End of file User.php */
-/* Location: ./application/models/User.php */
-
- ?>
+	 
+	 /* End of file user.php */
+	 /* Location: ./application/models/user.php */ ?>
